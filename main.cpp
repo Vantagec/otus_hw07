b3340eae0159b4280a4e96be1d86617f77102ce9
Vantagec <163841699+Vantagec@users.noreply.github.com>
2024-07-30 20:29:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,32 +6,24 @@
 #include <streamredirect.hpp>
 #include "version.h"
 
-// using namespace std;
-
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
     std::cout << "Bulk version: " << PROJECT_VERSION << std::endl;
 
-    const auto bulkSize = argc;
-    if (bulkSize != 2) {
+    if (argc != 2) {
         std::cout << "Usage:" << std::endl;
         std::cout << argv[0] << " [number of commands]" << std::endl;
         return 1;
     }
 
-    // обработка параметра командной строки с числом команд в блоке
     const int bulkCommandsLimit = std::atoi(argv[1]);
     if (bulkCommandsLimit < 1)
         return 0;
 
-    // обработка набора команд
-    auto cinCmdProcessing = [bulkCommandsLimit]()
-    {
+    auto processCommands = [bulkCommandsLimit]() {
         std::string strCommand;
         bulk_defs::CommandsController cmdProc{static_cast<std::size_t>(bulkCommandsLimit)};
 
         while (!cmdProc.isFinished() && std::cin >> strCommand) {
-            // std::cout << strCommand << std::endl;
             cmdProc.addCommand(std::exchange(strCommand, ""));
             using namespace std::chrono_literals;
             if (!cmdProc.isFinished())
@@ -39,53 +31,43 @@ int main(int argc, const char *argv[])
         }
     };
 
-    // examples
+    auto executeExample = [](const std::string& exampleName, std::istream& inputStream, auto& cmdProcessingFunc) {
+        std::cout << std::endl << exampleName << ":" << std::endl;
+        helpers::stream_redirect sr{std::cin, inputStream.rdbuf()};
+        cmdProcessingFunc();
+    };
 
-    { // обработка первого примера блока команд
-        
-        std::cout << std::endl << "example1: " << std::endl;
+    // Первый пример блока команд
+    std::istringstream example1Stream("cmd1\ncmd2\ncmd3\ncmd4\ncmd5\nEOF");
+    executeExample("example1", example1Stream, processCommands);
 
-        std::istringstream inputStream("cmd1\ncmd2\ncmd3\ncmd4\ncmd5\nEOF");
-        helpers::stream_redirect sr {std::cin, inputStream.rdbuf()};
+    // Второй пример блока команд
+    std::stringstream example2Stream;
+    example2Stream << "cmd1" << std::endl
+                   << "cmd2" << std::endl
+                   << "{" << std::endl
+                   << "cmd3" << std::endl
+                   << "cmd4" << std::endl
+                   << "}" << std::endl
+                   << "{" << std::endl
+                   << "cmd5" << std::endl
+                   << "cmd6" << std::endl
+                   << "{" << std::endl
+                   << "cmd7" << std::endl
+                   << "cmd8" << std::endl
+                   << "}" << std::endl
+                   << "cmd9" << std::endl
+                   << "}" << std::endl
+                   << "{" << std::endl
+                   << "cmd10" << std::endl
+                   << "cmd11" << std::endl
+                   << "EOF" << std::endl;
+    executeExample("example2", example2Stream, processCommands);
 
-        cinCmdProcessing();
-    
-    }
-
-    { // обработка второго примера блока команд
-        
-        std::cout << std::endl << "example2: " << std::endl;
-
-        std::stringstream inputStream; 
-        inputStream << "cmd1" << std::endl
-        << "cmd2" << std::endl
-        << "{" << std::endl
-        << "cmd3" << std::endl
-        << "cmd4" << std::endl
-        << "}" << std::endl
-        << "{" << std::endl
-        << "cmd5" << std::endl
-        << "cmd6" << std::endl
-        << "{" << std::endl
-        << "cmd7" << std::endl
-        << "cmd8" << std::endl
-        << "}" << std::endl
-        << "cmd9" << std::endl
-        << "}" << std::endl
-        << "{" << std::endl
-        << "cmd10" << std::endl
-        << "cmd11" << std::endl
-        << "EOF" << std::endl;
-
-        helpers::stream_redirect sr {std::cin, inputStream.rdbuf()};
-    
-        cinCmdProcessing();
-
-    }
-
-    std::cout << std::endl << "enter OEF to exit or enter the command:" << std::endl;
-
-    cinCmdProcessing();
+    // Основная обработка командного ввода
+    std::cout << std::endl << "enter EOF to exit or enter commands:" << std::endl;
+    processCommands();
 
     return 0;
 }
+
